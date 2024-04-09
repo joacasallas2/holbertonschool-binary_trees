5aa7453e1c04c5232cc6884e912a222c90771279
joacasallas2 <proyecta.ds@gmail.com>
2024-04-09 09:35:04 -0500

    function that creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,30 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: The pointer to the parent node of the node to create
+ * @value: The value to put in the new node
+ * Return: A pointer to the new node, or NULL on failure
+ * When created, a node does not have any child
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+	{
+		parent = malloc(sizeof(binary_tree_t));
+	}
+	new = malloc(sizeof(binary_tree_t));
+	if (parent != NULL && new != NULL)
+	{
+		new->n = value;
+		new->parent = parent;
+
+		if (new->n > parent->n)
+			parent->right = new;
+		else
+			parent->left = new;
+		return (new);
+	}
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -21,7 +21,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -41,7 +45,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second);
 
 
 #endif
